7487384f9b19720b9d5e310df6d4cbdf18516826
Edouard Quennesson <edouardquennesson@macbook-air-de-edouard.home>
2023-04-05 14:55:55 +0200

    Fixed the issue causing confirmation to not appear. This problem persist if the form is not valid the first time.

--- starterOnly/javascript/modal.js
+++ starterOnly/javascript/modal.js
@@ -1,3 +1,4 @@
+// Modifie le layout pour le responsive
 function editNav() {
   var nav = document.getElementById("myTopnav");
   if (nav.className === "topnav") {
@@ -15,6 +16,33 @@ const formData = document.querySelectorAll(".formData");
 const croixBtn = document.querySelector(".close");
 const formBtn = document.querySelector(".button");
 const closeC = document.querySelector(".close-confirm");
+const btnC = document.querySelector(".btn-confirm");
+
+//Validation form bouton
+formBtn.addEventListener("click", function (event){
+ 
+  validate();
+  //prevent l'envoie du formulaire pour pouvoir rendre visible la confirmation
+  event.preventDefault(); 
+  if(validity){
+
+    modalbgConfirm.style.visibility = "visible";
+    modalbg.style.display = "none";
+    console.log("là");
+
+  }
+  else{
+    console.log("ici");
+
+  }
+});
+
+btnC.addEventListener("click", function(){
+  modalbgConfirm.style.visibility = "hidden";
+  //je submit seulement après avoir cliquer sur fermer 
+  form.submit();
+
+});
 
 
 // launch modal event
@@ -36,155 +64,120 @@ function close() {
 function closeConfirm() {
   modalbgConfirm.style.display = "none";
 }
-//verification entrées utilisateur
-let validity = true;
 
 
-console.log(modalbgConfirm.style.visibility);
-console.log(modalbgConfirm.style.display);
 
-
-//Message d'erreur si remplissage faux
-const errorData = function (message) {
+//Message d'erreur si remplissage faux (formulaire)
+const errorData = function (i, message) {
   formData[i].setAttribute("data-error-visible", "true");
   formData[i].setAttribute("data-error", message);
   modalbgConfirm.style.display = "none";
-  
+
 
 }
- 
+
+//verification entrées utilisateur
+let validity = true;
+
 function validate() {
-console.log("je suis dans validate");
-console.log(modalbgConfirm.style.visibility);
+  console.log("je suis dans validate");
 
-//récupération des données utilisateurs
-const prenom = document.getElementById("first").value;
-const nom = document.getElementById("last").value;
-const mail = document.getElementById("email").value;
-// Regex pour l'adresse mail, gm indique une vérification globale
-const emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}', "gm");
-const dateNaissance = document.getElementById("birthdate").value;
-const nbrTournois = document.getElementById("quantity").value;
-const tournoisInt = parseInt(nbrTournois);
-const conditionsUser = document.getElementById("checkbox1").checked;
-
-
-  for (i = 0; i <= formData.length; i++) {
-
-    if (i === 0) {
-
-      if (prenom === "" || prenom.length < 2) {
-        errorData("Votre prénom doit contenir au moins 2 lettres");
-        validity = false;
-
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
-
-    if (i === 1) {
-
-      if (nom === "" || nom.length < 2) {
-        errorData("Votre nom doit contenir au moins 2 lettres");
-        validity = false;
-
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
+  // récupération des données utilisateurs
+  const prenom = document.getElementById("first").value;
+  const nom = document.getElementById("last").value;
+  const mail = document.getElementById("email").value;
+  // Regex pour l'adresse mail, gm indique une vérification globale
+  const emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}', "gm");
+  const dateNaissance = document.getElementById("birthdate").value;
+  const nbrTournois = document.getElementById("quantity").value;
+  const tournoisInt = parseInt(nbrTournois);
+  const conditionsUser = document.getElementById("checkbox1").checked;
 
 
-    if (i === 2) {
-
-      if (mail === "") {
-
-        errorData("Votre adresse mail doit être renseignée");
-        validity = false;
-
-      }
-      else if (!(mail.match(emailRegex))) {
-        errorData("Votre adresse mail est invalide");
-        validity = false;
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
-
-
-    if (i === 3) {
-      if (dateNaissance === "") {
-        errorData("Votre date de naissance n'est pas renseignée")
-        validity = false;
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
-
-
-
-
-    if (i === 4) {
-      if (isNaN(tournoisInt)) {
-        errorData("Vous devez saisir un nombre");
-        validity = false;
-      }
-      else if(tournoisInt < 0 || tournoisInt > 99){
-        errorData("Vous devez saisir un nombre entre 0 et 99");
-        validity = false;
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
-
-
-    if (i === 6) {
-
-      if (conditionsUser === false) {
-        errorData("Vous devez lire et accepter les conditions d'utilisation");
-        validity = false;
-
-      }
-      else {
-        formData[i].setAttribute("data-error-visible", "false");
-      }
-    }
-
-
+  if (prenom === "" || prenom.length < 2) {
+    errorData(0, "Votre prénom doit contenir au moins 2 lettres");
+    validity = false;
+  }
+  else {
+    formData[0].setAttribute("data-error-visible", "false");
+    validity = true;
   }
 
-  if (validity) {
 
-    console.log("true saved");
+  // if (nom === "" || nom.length < 2) {
+    
+  //   errorData(1, "Votre nom doit contenir au moins 2 lettres");
+  //   validity = false;
 
-    modalbgConfirm.style.visibility = "visible";
-    // modalbgConfirm.classList.add('confirm-active');
-  }
-  else{
-    console.log("validity est false");
-  }
-  console.log(modalbgConfirm.style.visibility);
+  // }
+  // else {
+  //   validity = true;
+  // }
 
+
+  // if (mail === "") {
+  //   i = 2
+  //   errorData("Votre adresse mail doit être renseignée");
+  //   validity = false;
+
+  // }
+  // else if (!(mail.match(emailRegex))) {
+  //   i = 2;
+  //   errorData("Votre adresse mail est invalide");
+  //   validity = false;
+  // }
+  // else {
+  //   formData[i].setAttribute("data-error-visible", "false");
+  // }
+
+  // if (dateNaissance === "") {
+  //   i = 3;
+  //   errorData("Votre date de naissance n'est pas renseignée")
+  //   validity = false;
+  // }
+  // else {
+  //   formData[i].setAttribute("data-error-visible", "false");
+  // }
+
+  // if (isNaN(tournoisInt)) {
+  //   i = 4;
+  //   errorData("Vous devez saisir un nombre");
+  //   validity = false;
+  // }
+  // else if (tournoisInt < 0 || tournoisInt > 99) {
+  //   errorData("Vous devez saisir un nombre entre 0 et 99");
+  //   validity = false;
+  // }
+  // else {
+  //   formData[i].setAttribute("data-error-visible", "false");
+  // }
+
+
+  // if (conditionsUser === false) {
+  //   i = 6;
+  //   errorData("Vous devez lire et accepter les conditions d'utilisation");
+  //   validity = false;
+
+  // }
+  // else {
+  //   formData[i].setAttribute("data-error-visible", "false");
+  // }
+
+  // if (validity) {
+
+  //   console.log("true saved");
+
+  //   modalbgConfirm.style.visibility = "visible";
+  //   // modalbgConfirm.classList.add('confirm-active');
+  // }
+  // else {
+  //   console.log("validity est false");
+  // }
   return validity;
 
 }
 
 
-//Form confirm (buttons)
-formBtn.addEventListener("click", validateConfirm);
-
-function validateConfirm() {
-
-
-   
-
-
-}
-
-
-
+console.log(validity);
+console.log(modalbgConfirm.style.visibility);
 
